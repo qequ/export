9addcf8ec5900ccc5a94fa36429f648352890d35
Alvaro Frias Garay <alvaro.frias@eclypsium.com>
2023-01-12 17:41:31 -0300

    Add test for nwdiag plantuml with components
    Signed-off-by: Alvaro Frias Garay <alvaro.frias@eclypsium.com>

--- src/test/java/com/structurizr/export/nwdiag/NwDiagExporterTests.java
+++ src/test/java/com/structurizr/export/nwdiag/NwDiagExporterTests.java
@@ -66,4 +66,58 @@ public class NwDiagExporterTests extends AbstractExporterTests {
         }
 
     }
+
+    @Test
+    public void test_NwDiagramExporterCreationPropertiesComponents() throws Exception {
+
+        Workspace workspace = WorkspaceUtils.loadWorkspaceFromJson(new File("./src/test/structurizr-nwdiag-components-workspace.json"));
+
+
+        NwDiagExporter exporter = new NwDiagExporter();
+        Collection<Diagram> diagrams = exporter.export(workspace);
+
+
+        for (Diagram d: diagrams) {
+            assertEquals(
+               "@startuml Deployment - oneNode\n" +
+                       "\n" +
+                       "<style>\n" +
+                       "nwdiagDiagram {\n" +
+                       "  group {\n" +
+                       "    BackGroundColor cadetblue\n" +
+                       "    #LineColor black\n" +
+                       "    #LineThickness 2.0\n" +
+                       "    #FontSize 11\n" +
+                       "    FontStyle bold\n" +
+                       "    #Margin 5\n" +
+                       "    #Padding 5\n" +
+                       "  }\n" +
+                       "}\n" +
+                       "</style>\n" +
+                       "nwdiag {\n" +
+                       "network anet {\n" +
+                       "Container_11;\n" +
+                       "Container_21;\n" +
+                       "}\n" +
+                       "network bnet {\n" +
+                       "Container_12;\n" +
+                       "Container_22;\n" +
+                       "}\n" +
+                       "group UnNode {\n" +
+                       "description = \"UnNode\"\n" +
+                       "  Container_11[color = \"Red\", description=\"<b>Container_11</b>\\n\\nComponent 112\\nComponent 111\\n\"]\n" +
+                       "  Container_12[color = \"Red\"]\n" +
+                       "  Container_21[color = \"Peru\"]\n" +
+                       "  Container_22[color = \"Peru\"]\n" +
+                       "}\n" +
+                       "\n" +
+                       "}\n" +
+                       "\n" +
+                       "@enduml",
+                    d.getDefinition()
+            );
+
+
+        }
+    }
 }
